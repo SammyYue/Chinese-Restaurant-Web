25d22e4ed9fedc39798306dbf5dfe4a8ae365da9
SammyYue <xu.yue09@hotmail.com>
2020-05-09 16:02:45 -0700

    sign

--- solution/js/script.js
+++ solution/js/script.js
@@ -85,10 +85,7 @@ showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
   buildAndShowHomeHTML,
-  loadMenuCategories,
-  loadMenuItems,
-  buildAndShowCategoriesHTML,
-  buildCategoriesViewHtml,// ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildMenuItemsViewHtml,// ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
