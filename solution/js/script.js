4e6c05e24a6c1b5b8ea84520bfc7c8a148906f2b
SammyYue <xu.yue09@hotmail.com>
2020-05-09 13:11:17 -0700

    sign

--- solution/js/script.js
+++ solution/js/script.js
@@ -84,7 +84,7 @@ var categoriesL = [{"id":81,"short_name":"L","name":"Lunch","special_instruction
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  buildAndShowHomeHTML(categoriesL),// ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowHomeHTML,// ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
